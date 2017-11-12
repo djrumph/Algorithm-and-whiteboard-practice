59ddb3726fabb1997e624090859adde46c19024c
Neoh213 <32970775+Neoh213@users.noreply.github.com>
2017-11-12 18:16:07 -0500

    Create Find the Longest Word in a String.java

---
+++ Find the Longest Word in a String.java
@@ -0,0 +1,30 @@
+class test{
+    public static void main(String[] args) {
+
+
+        System.out.println(findLongestWord("hello my friend"));
+
+
+    }
+
+    static int findLongestWord(String str)
+    {
+        //split String into array
+           String[] newStr = str.split(" ");
+        
+        //make variable to hold the value of the longest string
+        int longest = 0;
+        
+        
+        //goes through the array and find the longest string
+        for(int i =0; i<newStr.length; i++)
+        {
+
+            // if the current element is longer than the current longest element, it replaces that number with the current element. 
+            if (newStr[i].length() >longest)
+            {
+                longest = newStr[i].length();
+            }
+        }
+
+        return longest;
