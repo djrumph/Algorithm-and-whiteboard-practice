aed60c936f6a518cb8e60b5f80c758c7f9bf1668
Darryl Rumph, Jr <32970775+Neoh213@users.noreply.github.com>
2018-02-20 22:16:06 -0500

    Update Reverse String.js

--- Reverse String.js
+++ Reverse String.js
@@ -7,6 +7,20 @@ strArray.reverse();
   return strArray.join("");
 }
 
+//alternate way to do it
+
+function reverseString2(str){
+  
+    let reverseStr = "";
+    
+    for(var i = str.length-1; i>=0; i--){
+      
+      reverseStr= reverseStr + str[i];
+      console.log(str[i]);
+    }
+    return reverseStr;
+    
+  }
 
 
 reverseString("hello");
