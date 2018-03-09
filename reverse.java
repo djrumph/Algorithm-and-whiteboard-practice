d88cf9da621655f79d8530696e4e0f6283a82846
Darryl Rumph, Jr <32970775+Neoh213@users.noreply.github.com>
2018-03-09 12:49:52 -0500

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -17,4 +17,28 @@ public class Test {
         System.out.println(test);
         
         }
+    
+     public static void isPalin(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a String: ");
+
+        String str = input.next();
+
+        str = str.replaceAll("[1234567890!@#$%^&*()|?><;:]","");
+
+        StringBuilder reversing = new StringBuilder();
+
+        String reverseStr = reversing.append(str).reverse().toString();
+
+        System.out.println("Original String: " + str);
+        System.out.println("reverse String: " + reverseStr);
+
+        if(str.equals(reverseStr)){
+            System.out.println("Palindrone");
+        }
+        else{
+            System.out.println("not Palindrone");
+        }
+    }
   }
