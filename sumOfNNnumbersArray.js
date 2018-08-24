b1bfce3fbc54e62048021c1f8b47e5e41026f5c5
Darryl Rumph, Jr <32970775+djrumph@users.noreply.github.com>
2018-08-23 23:59:14 -0400

    Update sumOfNNnumbersArray.js

--- sumOfNNnumbersArray.js
+++ sumOfNNnumbersArray.js
@@ -1,14 +1,20 @@
 //function to find highest sum of n numbers in an array in O(n) time
 
 
-arr = [1,2,3,4,4,4,4,3,2,1];
+sumInArray( [1,2,3,4,4,4,4,3,2,1], 3);
 
-num = 4;
+function sumInArray (arr, num){
 
 maxSum = 0;
+high = 0;
 
-high = 10;
+//get the sum of the first n numbers at the beginning of the array
+for(let i = 0; i<num; i++){
+    high += arr[i];
+}
 
+
+//find the highest sum in the range of n numbers within the array
 for(let i = num ; i< arr.length; i++){
 
    // console.log(high);
@@ -19,6 +25,5 @@ for(let i = num ; i< arr.length; i++){
 
 }
 
-
-
-console.log(maxSum);
+return maxSum;
+}
