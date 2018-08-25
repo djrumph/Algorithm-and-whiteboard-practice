8d5be82cc5507215b03b4b1ea5fda3a75b80aca9
Darryl Rumph, Jr <32970775+djrumph@users.noreply.github.com>
2018-08-25 01:14:57 -0400

    Update frequencyDigit.js

--- frequencyDigit.js
+++ frequencyDigit.js
@@ -1,11 +1,36 @@
 // Given two positive integers find out if the two numbers have the same frequency of digets.
 
-//still working on it
-function numberToArray(num){
-    output = [],
-    sNumber = num.toString();
+function sameFrequency(num1, num2){
+  
+let  arr1= numberToArray(num1);
+let arr2 = numberToArray(num2);
 
-for (var i = 0, len = sNumber.length; i < len; i ++) {
+let obj1 = arryToObject(arr1);
+let obj2 = arryToObject(arr2);
+
+
+
+    if(arr1.length != arr2.length){
+        return false;
+    }
+
+    for (let val in obj1){
+
+
+        if(obj1[val] !== obj2[val]){
+            return false;
+        }
+    }
+
+
+    return true;
+}
+
+function numberToArray(num){
+let    output = [];
+let    sNumber = num.toString();
+
+for (let i = 0, len = sNumber.length; i < len; i ++) {
     output.push(+sNumber.charAt(i));
 }
 
@@ -15,44 +40,17 @@ return output;
 
 function arryToObject (arr){
 
-    ob ={};
+  let  ob ={};
 
-for(val of arr){
+for(let val of arr){
 
-    if(!ob[val]){
+  
         ob[val] = (ob[val] || 0) + 1;
-    }
+    
+    
 }
 
 return ob;
-
-
-}
-
-function sameDigits(ob1, ob2){
-
-
-arr1= numberToArray(123455);
-arr2 = numberToArray(554321);
-
-obj1 = arryToObject(arr1);
-obj2 = arryToObject(arr2);
-
-
-    if(arr1.length != arr2.length){
-        return false;
-    }
-
-    for (let val in obj1){
-
-        if(obj1[val] !== obj2[val]){
-            console.log("different number");
-            return false;
-        }
-    }
-
-    return true;
-
 
 
 }
