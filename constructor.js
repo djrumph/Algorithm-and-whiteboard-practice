c6622c3a26da60272992c38cf5b37acbf205aa29
Darryl Rumph, Jr <32970775+djrumph@users.noreply.github.com>
2018-09-08 14:31:16 -0400

    Create constructor.js

---
+++ constructor.js
@@ -0,0 +1,15 @@
+class Test{
+    constructor(subject, grade, hours){
+        this.student = subject;
+        this.grade = grade;
+        this.hours = hours;
+    }
+}
+
+let test1 = new Test("Calculus", "A", 4);
+
+let test2 = new Test("Algebra", "A", 4);
+
+
+console.log(test1)
+
