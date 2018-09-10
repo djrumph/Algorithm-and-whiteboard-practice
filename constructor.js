324dadad61c3ce02dd683a3f135f721273f5be11
Darryl Rumph, Jr <32970775+djrumph@users.noreply.github.com>
2018-09-09 20:27:45 -0400

    Update constructor.js

--- constructor.js
+++ constructor.js
@@ -1,9 +1,14 @@
 class Test{
     constructor(subject, grade, hours){
-        this.student = subject;
+        this.subject = subject;
         this.grade = grade;
         this.hours = hours;
     }
+
+    courseInfo(){
+          return `The course is:  ${this.subject}, the hours are: ${this.hours}, and your grade was: ${this.grade}`;
+    }
+
 }
 
 let test1 = new Test("Calculus", "A", 4);
@@ -11,5 +16,5 @@ let test1 = new Test("Calculus", "A", 4);
 let test2 = new Test("Algebra", "A", 4);
 
 
-console.log(test1)
+console.log(test1.courseInfo());
 
