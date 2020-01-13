20e23d6228c39808a3b6eef02fc62b19c2d53428
Darryl Rumph, Jr <32970775+djrumph@users.noreply.github.com>
2020-01-12 21:04:01 -0500

    Update AveragePair.js

--- AveragePair.js
+++ AveragePair.js
@@ -15,3 +15,24 @@ function averagePair(arr, avg){
     }
     return false;
 }
+
+function averagePair(arr, num){
+    let left = 0;
+    let right = arr.length-1;
+
+    while(left < right){
+        if((arr[right] + arr[left])/2 === num){
+            return true;
+        }
+        else if((arr[right] + arr[left])/2 > num){
+            right--;
+        }
+        else{
+            left++;
+        }
+
+    }
+
+    return false;
+
+}
