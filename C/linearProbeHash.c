bd034a46674a2250d5d9356407158b140ce02131
Darryl Rumph, Jr <32970775+djrumph@users.noreply.github.com>
2023-01-29 23:11:15 -0500

    Update linearProbeHash.c

--- C/linearProbeHash.c
+++ C/linearProbeHash.c
@@ -1,53 +1,33 @@
 // Type your code here, or load an example.
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+int main() {
 
-#define Empty 0
-int main(){
+    int list[]={-4,-4,3,6,-4};
 
-    int hashKey(int key);
+    int hash[10];
 
-    char word[] = "ahy";
+    for(int m =0; m<9; m++) hash[m] =0;
 
-    int h =0;
+    int count = 0;
+    int len = 10;
+    int hashNum;
 
-    int key = 0;
-    int salt =3;
+    while(count < 5){
+        hashNum = list[count] % len +1;
 
-    int list[] = {-4,2,-4,4,8,-3};
-    int hash[5];
+        // printf("%d\n", hash[abs(hashNum)]);
 
-    for(int f = 0; f<5; f++) hash[f] = 0;
-
-    int*listP = list;
-    int hashNum = 0;
-    int distinct = 0;
-
-    int arrayLength = 6;
-
-    while(distinct < arrayLength){
-        printf("value: %d\n", *listP);
-        hashNum  = *listP % arrayLength +1;
-        while(hash[abs(hashNum)] != 0 & hash[abs(hashNum)] != 0){
-            hashNum = hashNum % arrayLength+1;
-        }
-        if(distinct == arrayLength){
-            printf("Table Full\n");
-            break;
+        while(hash[abs(hashNum)] !=0  ) {
+            hashNum = hashNum +1;
+            printf("hash collison for num: %d at hash: %d\n", list[count], hashNum);
         }
 
-        hash[abs(hashNum)]= *listP;
-        printf("value: %d\n", *listP);
+        hash[abs(hashNum)] = list[count];
 
-        printf("hashed index: %d\n", abs(hashNum));
-        printf("hashed value: %d\n", hash[abs(hashNum)]);
-        distinct++;
-        listP+=1;
+        printf("hash: %d, num: %d\n", abs(hashNum), hash[abs(hashNum)]);
+
+        count++;
     }
-
-    for(int g = 0; g <6; g++){
-        printf("%d\n", hash[g]);
-    }
-
+    return 0;
 }
